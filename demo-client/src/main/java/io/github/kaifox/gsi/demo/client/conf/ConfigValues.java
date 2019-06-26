5f7ddaf0c86308316a48d5d09feee16be5893083
kaifox <kajetan.fuchsberger@gmail.com>
2019-06-26 10:45:46 +0200

    passing through command line args to application when starting with gradle

--- demo-client/src/main/java/io/github/kaifox/gsi/demo/client/conf/ConfigValues.java
+++ demo-client/src/main/java/io/github/kaifox/gsi/demo/client/conf/ConfigValues.java
@@ -1,6 +1,11 @@
 package io.github.kaifox.gsi.demo.client.conf;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class ConfigValues {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigValues.class);
 
     public static final String HOST = "localhost";
     private static final int HTTP_PORT = 8080;
@@ -15,6 +20,7 @@ public final class ConfigValues {
     public static int httpPort() {
         String port = System.getProperty("server.port");
         if (port == null) {
+            LOGGER.info("Property 'server.port' not set. Returning default port (" + HTTP_PORT + ")");
             return HTTP_PORT;
         } else {
             return Integer.valueOf(port);
